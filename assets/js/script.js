c5deb4f33e2d34cc2938989d1adb5731ed1c393a
Isaac <you@example.com>
2022-02-11 15:57:55 +0200

    3rd js commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -124,6 +124,21 @@ var VoiceRSS = {
 
 // test();
 
+// Passing Joke to VoiceRSS API
+function tellMe(joke) {
+  console.log("tell me:", joke);
+  VoiceRSS.speech({
+    key: "3c099b59f35542a38890404cc58b3ab0",
+    src: joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
+  });
+}
+
 // Get Jokes from Joke API
 async function getJokes() {
   let joke = "";
@@ -131,13 +146,13 @@ async function getJokes() {
   try {
     const response = await fetch(apiUrl);
     const data = await response.json();
-    // console.log(data);
+
     if (data.type === "twopart") {
       joke = `${data.setup}...${data.delivery}`;
-      console.log(joke);
     } else {
-      console.log(data.joke);
+      joke = data.joke;
     }
+    tellMe(joke);
   } catch (error) {
     console.log("Whpoos", error);
   }
